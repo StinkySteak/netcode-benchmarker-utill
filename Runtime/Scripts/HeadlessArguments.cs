93a5b8573e8eb85d7eb91d2873565ddf92d02ba8
StinkySteak <slimesteve17@gmail.com>
2024-03-18 21:44:11 +0700

    feat: add headless arguments

---
+++ Runtime/Scripts/HeadlessArguments.cs
@@ -0,0 +1,10 @@
+namespace StinkySteak.NetcodeBenchmark
+{
+    public static class HeadlessArguments
+    {
+        public const string AUTO_CLIENT = "-autoclient";
+        public const string AUTO_SERVER = "-autoserver";
+        public const string CLIENT_COUNT = "-clientcount";
+        public const string SERVER_IP = "-serverip";
+    }
+}
